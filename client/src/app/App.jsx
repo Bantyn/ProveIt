9ea58fc2588c26ff144c81f36d7fb5804267613d
Bantyn <patelbanty1260@gmail.com>
2025-12-24 03:10:52 +0530

    Test Done !

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -1,7 +1,11 @@
-import ErrorBoundary from './ErrorBoundary.jsx';
+
+import ErrorBoundary from './ErrorBoundary.jsx'
 import { routes } from "./routes.jsx";
-import { useRoutes, useLocation } from "react-router-dom";
-import '../styles/App.css';
+// import { ThemeProvider } from "next-themes";
+import { useRoutes,useLocation } from "react-router-dom";
+import '../styles/App.css'
+import Footer from '../components/ui/footer/Footer.jsx'
+import Navbar from '../components/ui/navbar/Navbar.jsx';
 
 export default function App() {
   const element = useRoutes(routes);
@@ -9,32 +13,28 @@ export default function App() {
   const hideHeaderFooterPaths = ["/signup/companySignup", "/signin/companySignin","/signup/companySignup/plan_selection","/masterLogin"];
 
   const showHeaderFooter = !hideHeaderFooterPaths.includes(location.pathname);
-
   return (
     <>
-      <ErrorBoundary>
+    {/* <ThemeProvider> */}
+     <ErrorBoundary>
+      {showHeaderFooter && (
+      <header className=''>
+        <Navbar></Navbar>
+      </header>)}
 
-        {showHeaderFooter && (
-          <header>
-            <h1 className="dark:text-white text-center py-5 bg-white dark:bg-black border-b border-black/10 fixed z-50 w-full top-0">
-              ProveIt header
-            </h1>
-          </header>
-        )}
+      <main className='min-h-screen'>
+      {element}
+      </main>
+      
+      {showHeaderFooter && (
+      <footer className='mt-24 border-t-1 border-gray-500/20  transition-all duration-200 dark:border-amber-50/10'>
+          <Footer></Footer>
+      </footer>)}
 
-        <main className=''>
-          {element}
-        </main>
-
-        {showHeaderFooter && (
-          <footer className='mt-50'>
-            <h1 className="dark:text-white text-center py-5 bg-white dark:bg-black border-t border-black/10 h-50 pt-10">
-              ProveIt footer
-            </h1>
-          </footer>
-        )}
-
-      </ErrorBoundary>
+    </ErrorBoundary>
+    {/* </ThemeProvider> */}
     </>
-  );
+  )
 }
+
+
--- client/src/app/routes.jsx
+++ client/src/app/routes.jsx
@@ -2,11 +2,7 @@
 import Login from "../features/auth/login/Login";
 //import ClientSignup from "../features/auth/signup/client/ClientSignup";
 import CompanySignup from "../features/auth/signup/company/CompanySignup";
-import CompanySignin from "../features/auth/signup/company/CompanyAuth/Logn";
-import PlanSelection from "../features/auth/signup/company/PricingSection";
-import ContactPage from "../features/pages/Contact/Contact";
-
-// import register from "../features/pages/CompanyAuth/Register"
+import EmployeeProfile from "../features/pages/Emplyee_UI/profile/Profile";
 
 // Pages
 import Home from "../features/pages/Emplyee_UI/dashboard/EmplyeeDashboard";
@@ -34,18 +30,8 @@ export const routes = [
     path: "/signup/companySignup",
     element: <CompanySignup />,
   },
-
   {
-    path: "/signin/companySignin",
-    element: <CompanySignin />,
+    path: "/employee/employeeProfile",
+    element: <EmployeeProfile />,
   },
-  {
-    path: "/contact",
-    element: <ContactPage />,
-  },
-
-   {
-    path: "/signup/companySignup/plan_selection",
-    element: <PlanSelection />,
-  }
 ];
