9253db4fda5859245f7dc481b1ddba4163f64bc5
Bantyn <patelbanty1260@gmail.com>
2025-12-24 03:21:21 +0530

    Done

--- client/src/app/routes.jsx
+++ client/src/app/routes.jsx
@@ -1,12 +1,15 @@
 // Authanication
 import Login from "../features/auth/login/Login";
-//import ClientSignup from "../features/auth/signup/client/ClientSignup";
+import ClientSignup from "../features/auth/signup/client/ClientSignup";
 import CompanySignup from "../features/auth/signup/company/CompanySignup";
 import EmployeeProfile from "../features/pages/Emplyee_UI/profile/Profile";
 
 // Pages
 import Home from "../features/pages/Emplyee_UI/dashboard/EmplyeeDashboard";
 import Landing from "../features/pages/landing/Landing";
+// import { Contact } from "lucide-react";
+import Contact from "../features/pages/Contact/Contact";
+
 
 export const routes = [
    {
@@ -17,15 +20,14 @@ export const routes = [
     path: "/client/emplyee_dashboard",
     element: <Home />,
   },
-  
   {
-    path: "/login",
+    path: "/masterLogin",
     element: <Login />,
   },
-  // {
-  //   path: "/signup/clientSignup",
-  //   element: <ClientSignup />,
-  // },
+  {
+    path: "/signup/clientSignup",
+    element: <ClientSignup />,
+  },
   {
     path: "/signup/companySignup",
     element: <CompanySignup />,
@@ -34,4 +36,8 @@ export const routes = [
     path: "/employee/employeeProfile",
     element: <EmployeeProfile />,
   },
+  {
+    path: "/contact",
+    element: <Contact />,
+  }
 ];
