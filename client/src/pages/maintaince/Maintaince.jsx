a95222cc77845b5011e26b05fa5cb632c29391cd
Bantyn <patelbanty1260@gmail.com>
2025-12-31 20:31:19 +0530

    Maintanice

--- client/src/pages/maintaince/Maintaince.jsx
+++ client/src/pages/maintaince/Maintaince.jsx
@@ -5,7 +5,7 @@ export default function Maintaince() {
     <>
         <div className="relative h-screen w-full overflow-hidden">
             <Spline scene="https://prod.spline.design/1KFTlhzaTgEZ0aN1/scene.splinecode" className='absolute h-full w-full' />
-            <div className="absolute h-15 w-full bg-white bottom-0"></div>
+            <div className="fixed h-11 w-36 rounded-xl bg-gradient-to-r from-violet-200  via-pink-300 to-orange-300 bottom-4 right-4 flex items-center justify-center overflow-hidden font-bold text-white text-1xl uppercase"> ProveIt.io</div>
         </div>
 
     </>
